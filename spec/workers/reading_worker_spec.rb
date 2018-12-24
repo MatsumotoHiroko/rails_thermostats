8b44394e1a353a0815a3192d031e556cc8332485
matsumoto <hm.hiroko.matsumoto@gmail.com>
2018-12-24 19:07:29 +0900

    ReadingWorker spec : perform added

--- spec/workers/reading_worker_spec.rb
+++ spec/workers/reading_worker_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
+require 'pry'
 RSpec.describe ReadingWorker, type: :worker do
-  let(:reading){ build(:reading) }
   it { expect(ReadingWorker).to be_processed_in :reading }
+  let(:thermostat){ create(:thermostat) }
+  describe "#perform" do
+    let(:temperature) { 1.0 }
+    let(:humidity) { 2.0 }
+    let(:battery_charge) { 3.0 }
+
+    subject { 
+      work = ReadingWorker.new
+      work.perform({thermostat_id: thermostat.id,
+        temperature: temperature,
+        humidity: humidity, 
+        battery_charge: battery_charge})
+    }
+    
+    describe "success" do
+      it { expect { subject }.to change { Reading.all.size }.from(0).to(1) }
+      it "create Reading" do
+        expect { subject }
+          .to change { Reading.first&.slice(:thermostat_id, :temperature, :humidity, :battery_charge) }
+          .from(nil)
+          .to(
+             {
+               thermostat_id: thermostat.id,
+               temperature: temperature,
+               humidity: humidity,
+               battery_charge: battery_charge
+            }
+          )
+      end
+    end
+  end
 end
